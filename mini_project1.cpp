8879c9b2163219a355bcda8ea049f51c1da9cb40
abishekpv24it-prog <abishekpv.24it@kongu.edu>
2026-06-04 09:52:47 +0530

    Add files via upload

---
+++ mini_project1.cpp
@@ -0,0 +1,80 @@
+#include <stdio.h>
+
+int balance = 5000;
+
+void deposit()
+{
+    int amt;
+    printf("Enter Amount to Deposit: ");
+    scanf("%d", &amt);
+
+    balance += amt;
+
+    printf("Amount Deposited Successfully!\n");
+    printf("Current Balance = %d\n", balance);
+}
+
+void withdraw()
+{
+    int amt;
+
+    printf("Enter Amount to Withdraw: ");
+    scanf("%d", &amt);
+
+    if (amt <= balance)
+    {
+        balance -= amt;
+        printf("Amount Withdrawn Successfully!\n");
+        printf("Current Balance = %d\n", balance);
+    }
+    else
+    {
+        printf("Insufficient Balance!\n");
+    }
+}
+
+void showBalance()
+{
+    printf("Current Balance = %d\n", balance);
+}
+
+int main()
+{
+    int choice;
+
+    do
+    {
+        printf("\n===== BANK MENU =====\n");
+        printf("1. Deposit\n");
+        printf("2. Withdraw\n");
+        printf("3. Show Balance\n");
+        printf("4. Exit\n");
+        printf("Enter Your Choice: ");
+        scanf("%d", &choice);
+
+        switch (choice)
+        {
+            case 1:
+                deposit();
+                break;
+
+            case 2:
+                withdraw();
+                break;
+
+            case 3:
+                showBalance();
+                break;
+
+            case 4:
+                printf("Thank You!\n");
+                break;
+
+            default:
+                printf("Invalid Choice!\n");
+        }
+
+    } while (choice != 4);
+
+    return 0;
+}
